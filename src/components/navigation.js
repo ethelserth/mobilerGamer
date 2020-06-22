443975d8df31b4e80fb978749ace402c17dac4c0
ethelserth <pergaminh@live.com>
2020-06-22 20:54:36 +0300

    review - news pages add

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,6 +5,8 @@ import ThemeChanger from "../components/themeChanger"
 export default (props) => (
   <nav className="navigation"> 
     <Link to="/contact">Contact</Link>
+    <Link to="/reviews">Reviews</Link>
+    <Link to="/news">News</Link>
     <ThemeChanger/>
   </nav>
   
